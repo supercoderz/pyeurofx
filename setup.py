df58b0720e1f2d4a3ef084ee334a6621d7c3f5ba
Narahari Allamraju <anarahari@gmail.com>
2014-11-12 19:48:10 +0800

    tweaked setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@
 from distutils.core import setup
 
 setup(name='pyeurofx',
-      version='0.3',
-      description='Python module to get and parse daily and historical FX rates from ECB',
-      long_description='Please visit https://github.com/supercoderz/pyeurofx for more details',
-      author='Narahari Allamraju',
-      author_email='anarahari@gmail.com',
-      url='https://github.com/supercoderz/pyeurofx',
-      packages=['eurofx'],
-      requires=['lxml','requests','pandas'],
-     )
+	  version='0.3',
+	  description='Python module to get and parse daily and historical FX rates from ECB',
+	  long_description='Please visit https://github.com/supercoderz/pyeurofx for more details',
+	  author='Narahari Allamraju',
+	  author_email='anarahari@gmail.com',
+	  url='https://github.com/supercoderz/pyeurofx',
+	  packages=['eurofx'],
+	  requires=['lxml','requests','pandas'],
+	classifiers = [
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3',
+	]
+	 )
