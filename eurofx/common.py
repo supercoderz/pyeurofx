b2ac2bb5dd9de4f5d97a6ab5e7caa580d567b3c1
Narahari <anarahari@gmail.com>
2015-04-04 10:06:58 +0800

    fixed pandas issue

--- eurofx/common.py
+++ eurofx/common.py
@@ -6,10 +6,13 @@ import datetime
 HISTORICAL='https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml'
 DAILY='https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml'
 
-def get_and_parse(data_url):
+def get_and_parse(data_url,use_pandas=False):
     result = requests.get(data_url)
     if result.status_code == requests.codes.ok:
-        return parse_and_load(result.content)
+        if use_pandas:
+            return parse_and_load_df(result.content)
+        else:
+            return parse_and_load(result.content)
         
 def parse_and_load(content):
     data = []
@@ -26,6 +29,39 @@ def parse_and_load(content):
             date = get_date(time)
             data.append((currency,date,rate))
     return data
-                
+
+def parse_and_load_df(content):
+    data = []
+    doc = etree.XML(content)
+    nodes = doc.iterchildren()
+    subject = nodes.__next__()
+    sender = nodes.__next__()
+    parent_cube = nodes.__next__()
+    times,currencies = get_index_and_cols(parent_cube)
+    df = pandas.DataFrame(index=times,columns=currencies)
+    load_dataframe(df,parent_cube)
+    return df
+    
+
+def load_dataframe(df,node):
+    for daily_cube in node.iterchildren():
+        time = daily_cube.attrib['time']
+        for currency_cube in daily_cube.iterchildren():
+            currency = currency_cube.attrib['currency']
+            rate = currency_cube.attrib['rate']
+            df.ix[time,currency] = rate
+
+def get_index_and_cols(node):
+    times = []
+    currencies = []
+    for daily_cube in node.iterchildren():
+        time = daily_cube.attrib['time']
+        times.append(time)
+        for currency_cube in daily_cube.iterchildren():
+            currency = currency_cube.attrib['currency']
+            if currency not in currencies:
+                currencies.append(currency)
+    return (times,currencies)
+                    
 def get_date(date_string):
     return datetime.datetime.strptime(date_string, "%Y-%m-%d").date()
--- eurofx/eurofx_pandas.py
+++ eurofx/eurofx_pandas.py
@@ -3,45 +3,8 @@ import datetime
 import pandas
 from .common import *
 
-#this is overridden here        
-def parse_and_load(content):
-    data = []
-    doc = etree.XML(content)
-    nodes = doc.iterchildren()
-    subject = nodes.__next__()
-    sender = nodes.__next__()
-    parent_cube = nodes.__next__()
-    times,currencies = get_index_and_cols(parent_cube)
-    df = pandas.DataFrame(index=times,columns=currencies)
-    load_dataframe(df,parent_cube)
-    return df
-    
-
-def load_dataframe(df,node):
-    for daily_cube in node.iterchildren():
-        time = daily_cube.attrib['time']
-        for currency_cube in daily_cube.iterchildren():
-            currency = currency_cube.attrib['currency']
-            rate = currency_cube.attrib['rate']
-            df.ix[time,currency] = rate
-
-def get_index_and_cols(node):
-    times = []
-    currencies = []
-    for daily_cube in node.iterchildren():
-        time = daily_cube.attrib['time']
-        times.append(time)
-        for currency_cube in daily_cube.iterchildren():
-            currency = currency_cube.attrib['currency']
-            if currency not in currencies:
-                currencies.append(currency)
-    return (times,currencies)
-                    
-def get_date(date_string):
-    return datetime.datetime.strptime(date_string, "%Y-%m-%d").date()
-
 def get_historical_data_df():
-    return get_and_parse(HISTORICAL)
+    return get_and_parse(HISTORICAL,use_pandas=True)
 
 def get_daily_data_df():
-    return get_and_parse(DAILY)
+    return get_and_parse(DAILY,use_pandas=True)
