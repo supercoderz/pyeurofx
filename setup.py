f443e54514d915937235634b4da04ecf24f05b1e
Narahari <anarahari@gmail.com>
2015-04-04 09:43:49 +0800

    fixed install requires

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(name='pyeurofx',
-	  version='0.3',
+	  version='0.3.1',
 	  description='Python module to get and parse daily and historical FX rates from ECB',
 	  long_description='Please visit https://github.com/supercoderz/pyeurofx for more details',
 	  author='Narahari Allamraju',
 	  author_email='anarahari@gmail.com',
 	  url='https://github.com/supercoderz/pyeurofx',
 	  packages=['eurofx'],
-	  requires=['lxml','requests','pandas'],
+	  install_requires=['lxml','requests','pandas'],
 	classifiers = [
 		'Programming Language :: Python :: 2.7',
 		'Programming Language :: Python :: 3.4',
